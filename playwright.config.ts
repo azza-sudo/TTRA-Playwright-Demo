d6d467c74acc1efd59a09ce8472ac59f9cb82aa5
Azza Salah El Din <salaha@extendad.com>
2025-10-31 14:46:12 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['json', { outputFile: 'playwright-report/sample_results.json' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
  timeout: 60000,
   use: {
